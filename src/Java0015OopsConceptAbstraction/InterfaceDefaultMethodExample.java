e6784d2d9742a01990f203cfae36207a0c6c9426
Vaibhav Chaurasia <vchaurasia@dminc.com>
2022-07-17 22:55:38 +0530

    Add OOps Concepts - Abstraction

---
+++ src/Java0015OopsConceptAbstraction/InterfaceDefaultMethodExample.java
@@ -0,0 +1,25 @@
+package Java0015OopsConceptAbstraction;
+
+public class InterfaceDefaultMethodExample {
+	public static void main(String args[]){  
+		Drawable1 d = new Rectangle1();  
+		
+		d.draw();  
+		d.msg();  
+	}
+}
+
+interface Drawable1{
+
+	void draw();
+
+	default void msg(){
+		System.out.println("default method");
+	}
+}
+
+class Rectangle1 implements Drawable1{  
+	public void draw(){
+		System.out.println("drawing rectangle");
+	}
+} 
