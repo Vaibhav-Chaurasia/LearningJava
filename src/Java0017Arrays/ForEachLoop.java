78eec769de8084e3e7fda7dd23cb0a0361214e69
Vaibhav Chaurasia <vchaurasia@dminc.com>
2022-07-19 11:59:39 +0530

    Arrays

---
+++ src/Java0017Arrays/ForEachLoop.java
@@ -0,0 +1,11 @@
+package Java0017Arrays;
+
+public class ForEachLoop {
+	public static void main(String args[]){  
+		int arr[] = {33, 3, 4, 5};  
+
+		//printing array using for-each loop  
+		for(int i : arr)  
+			System.out.println(i);  
+	}
+}
