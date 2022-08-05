485d8e020d64199dd4a6864357506abb75f15a9c
Vaibhav Chaurasia <vchaurasia@dminc.com>
2022-08-05 20:55:07 +0530

    Small Tweaks

--- src/Java0012OopsConceptObjectsAndClasses/StaticNonStaticExample.java
+++ src/Java0012OopsConceptObjectsAndClasses/StaticNonStaticExample.java
@@ -2,7 +2,7 @@ package Java0012OopsConceptObjectsAndClasses;
 
 public class StaticNonStaticExample {
 
-	int a = 40;//non static  
+	int a = 40; //non static  
 
 	public static void main(String args[]){  
 		//System.out.println(a);  
